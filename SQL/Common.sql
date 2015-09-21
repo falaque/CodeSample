477f7b35b3a3002c5dcf68e16a2f56a4ecd1f329
falaque <falaque@users.noreply.github.com>
2015-09-21 20:25:25 +0530

    Update Common.sql

--- SQL/Common.sql
+++ SQL/Common.sql
@@ -5,6 +5,10 @@ SET @NewSiteBudgetAmount = -1;
      
      IF(@NewSiteBudgetAmount<0)
      BEGIN
-		  RAISERROR('There is an exception.',17,1);
+		  RAISERROR('There is an exception.',16,1);
+		  RETURN; -- you need to return explicitly or execution wont stop.
      END
+/*
+- Read more on RaiseError: http://www.sommarskog.se/error-handling-I.html
+*/
 -- Raise Error
